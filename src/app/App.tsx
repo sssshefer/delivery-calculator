ad4fa128d9ec84d81302861cf11bbbbfae33be83
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-01-20 13:18:50 +0200

    Omnes font and SCSS support are added. Basic CSS styles are set

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-     Hi
-    </div>
-  );
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
